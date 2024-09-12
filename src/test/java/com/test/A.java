6c4be449ee8e7efc47dc58a7b1b824ae19a15be1
Amrithaa15 <amrithaa15598@gmail.com>
2024-09-12 13:21:20 +0530

    New project

---
+++ src/test/java/com/test/A.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class A {
+	private void tc1() {
+		System.out.println(1);
+
+	}
+	private void tc2() {
+		System.out.println(2);
+
+	}
+	private void tc3() {
+		System.out.println(3);
+	}
+}
